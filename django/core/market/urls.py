152b28249a15f5a24b55ddafba4df3ad035533a3
S-CyberTech <SantiagoCTech@gmail.com>
2024-12-03 22:05:45 -0300

    .

--- django/core/market/urls.py
+++ django/core/market/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from market.views import search_engine_mt1, search_engine_mt2, get_search_engine_models
+# from market.views import search_engine_mt1, search_engine_mt2, get_search_engine_models
+from market.views import search_engine_mt1, get_search_engine_models
 from market.data_collection.views import load_categories_from_json, CleanDuplicateCategories
 
 urlpatterns = [
@@ -7,6 +8,6 @@ urlpatterns = [
     #path('market/clean-duplicates/', CleanDuplicateCategories, name='clean_duplicates'),
 
     path('market/category/MT1/search/', search_engine_mt1, name='search_engine_mt1'),
-    path('market/category/MT2/search/', search_engine_mt2, name='search_engine_mt2'),
+    # path('market/category/MT2/search/', search_engine_mt2, name='search_engine_mt2'),
     path('search-engine-models/', get_search_engine_models, name='get_search_engine_models'),
 ]
--- django/core/market/views.py
+++ django/core/market/views.py
@@ -1,6 +1,6 @@
 import json
 from datetime import date
-from fuzzywuzzy import fuzz
+# from fuzzywuzzy import fuzz
 from .models import Category
 from django.db.models import Q
 from django.views import View
@@ -34,62 +34,62 @@ def search_engine_mt1(request):
 
 
 
-def search_engine_mt2(request):
-    query = request.GET.get('query', '').strip()
+# def search_engine_mt2(request):
+#     query = request.GET.get('query', '').strip()
     
-    if not query:
-        return JsonResponse({'results': []})
+#     if not query:
+#         return JsonResponse({'results': []})
 
-    categories = Category.objects.all()
-    results = []
+#     categories = Category.objects.all()
+#     results = []
 
-    # Busca coincidencias utilizando fuzzywuzzy
-    for category in categories:
-        score = fuzz.ratio(query.lower(), category.name.lower())
-        if score >= 55:
-            results.append({
-                'id': category.id,
-                'name': category.name,
-                'path_from_root': category.path_from_root,
-                'parent': category.parent.id if category.parent else None,
-                'score': score,
-            })
+#     # Busca coincidencias utilizando fuzzywuzzy
+#     for category in categories:
+#         score = fuzz.ratio(query.lower(), category.name.lower())
+#         if score >= 55:
+#             results.append({
+#                 'id': category.id,
+#                 'name': category.name,
+#                 'path_from_root': category.path_from_root,
+#                 'parent': category.parent.id if category.parent else None,
+#                 'score': score,
+#             })
     
-    # Ordena los resultados por la puntuación de la coincidencia
-    results = sorted(results, key=lambda x: x['score'], reverse=True)
+#     # Ordena los resultados por la puntuación de la coincidencia
+#     results = sorted(results, key=lambda x: x['score'], reverse=True)
 
-    # Agrupa los resultados por el 'path_from_root'
-    grouped_results = {}
-    for result in results:
-        path_with_score = [
-            {'id': entry['id'], 'name': entry['name'], 'score': result['score']}
-            for entry in result['path_from_root']
-        ]
+#     # Agrupa los resultados por el 'path_from_root'
+#     grouped_results = {}
+#     for result in results:
+#         path_with_score = [
+#             {'id': entry['id'], 'name': entry['name'], 'score': result['score']}
+#             for entry in result['path_from_root']
+#         ]
         
-        path_tuple = tuple((entry['id'], entry['name']) for entry in result['path_from_root'])
-        if path_tuple not in grouped_results:
-            grouped_results[path_tuple] = {
-                'path_from_root': path_with_score,
-                'categorie': []
-            }
+#         path_tuple = tuple((entry['id'], entry['name']) for entry in result['path_from_root'])
+#         if path_tuple not in grouped_results:
+#             grouped_results[path_tuple] = {
+#                 'path_from_root': path_with_score,
+#                 'categorie': []
+#             }
         
-        grouped_results[path_tuple]['categorie'].append(result)
+#         grouped_results[path_tuple]['categorie'].append(result)
 
-    # Construye la respuesta
-    response = {
-        'categories_from_root': [
-            {
-                'path_from_root': entry['path_from_root'],
-                'score': entry['path_from_root'][0]['score']
-            }
-            for entry in grouped_results.values()
-        ],
-        'categories': [
-            {'categorie': entry['categorie']} for entry in grouped_results.values()
-        ]
-    }
+#     # Construye la respuesta
+#     response = {
+#         'categories_from_root': [
+#             {
+#                 'path_from_root': entry['path_from_root'],
+#                 'score': entry['path_from_root'][0]['score']
+#             }
+#             for entry in grouped_results.values()
+#         ],
+#         'categories': [
+#             {'categorie': entry['categorie']} for entry in grouped_results.values()
+#         ]
+#     }
 
-    return JsonResponse(response)
+#     return JsonResponse(response)
 
 def get_search_engine_models(request):
     models = [
